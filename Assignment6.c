5ced06d406a8600366158a64ad6fced029fa47c2
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-04-08 16:20:36 +0530

    Update Assignment6.c

--- Assignment6.c
+++ Assignment6.c
@@ -2,11 +2,12 @@
 #include<math.h>
 int main()
 {
-	int n,x;
+	int n;
+	float x;
 	printf("Enter a number\n");
 	scanf("%d", &n);
-	x = (2^n)/(fact(n));
-	printf("x is %d\n",x);
+	x = (power(2,n))/(fact(n));
+	printf("x is %f\n",x);
 }
 int fact(int n)
 {	
@@ -17,5 +18,16 @@ int fact(int n)
 	else
 	{
 		return(n*fact(n-1));	
+	}
+}
+int power(int a, int b)
+{
+	if(b == 1)
+	{
+		return 2;
+	}
+	else
+	{
+	return (a*power(a,b-1));	
 	}
 }
