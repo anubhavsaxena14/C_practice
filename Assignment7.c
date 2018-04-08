d33e65979c1be0c75f8d77d897558e9ae19bb074
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-04-08 16:43:05 +0530

    Create Assignment7.c

---
+++ Assignment7.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	float x;
+	printf("Enter a number\n");
+	scanf("%d", &n);
+	x = (power(2,2*n+1))/(fact(2*n));
+	printf("x is %f\n",x);
+}
+int fact(int n)
+{	
+	if(n == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return(n*fact(n-1));	
+	}
+}
+int power(int a, int b)
+{
+	if(b == 1)
+	{
+		return 2;
+	}
+	else
+	{
+	return (a*power(a,b-1));	
+	}
+}
